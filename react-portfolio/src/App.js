b2c00e57721eb77289c1702fb116bcbe2bca79fd
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-10-15 13:25:03 -0300

    feat: Adicionar navbar chore: Mudando versão do tailwind

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div className="App">
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import NavBar from "./scenes/Navbar";
 
+function App() {
+  // Estado para armazenar a página selecionada, iniciando com "home"
+  const [selectedPage, setSelectedPage] = useState("home") ;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <div className="App bg-deep-blue">
+      <NavBar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ react-portfolio/src/scenes/Navbar.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
+
+  return (
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">ISAAC</h4>
+
+        {/* Para PC */}
+        {isDesktop ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Início"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Habilidades"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projetos"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contato"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img alt="menu-icon" src="../assets/menu-icon.svg" />
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
