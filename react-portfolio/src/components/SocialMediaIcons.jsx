38e312dad17293f93b31a85e77b29f1b3732f86a
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-11-18 17:14:55 -0300

    feat: atualizar links para incluir linkedin e instagram

--- react-portfolio/src/components/SocialMediaIcons.jsx
+++ react-portfolio/src/components/SocialMediaIcons.jsx
@@ -8,31 +8,31 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com"
+        href="https://www.linkedin.com/in/isaaclusca/"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="linkedin-link" src={linkedinImg} />
       </a>
-      <a
+      {/* <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.twitter.com"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="twitter-link" src={twitterImg} />
-      </a>
-      <a
+      </a> */}
+      {/* <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.facebook.com"
         target="_blank"
         rel="noreferrer"
       >
         <img alt="facebook-link" src={facebookImg} />
-      </a>
+      </a> */}
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com"
+        href="https://www.instagram.com/isaaclusca/"
         target="_blank"
         rel="noreferrer"
       >
